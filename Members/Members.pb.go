0dfc7613f7104f3831e27fa1a9fbfc0c654109b5
Thomas Bouder <tbouder@protonmail.com>
2020-02-14 16:48:32 +0100

    Add getMember proto

--- Members/Members.pb.go
+++ Members/Members.pb.go
@@ -231,6 +231,84 @@ func (m *LoginMemberResponse) GetHashKey() string {
 	return ""
 }
 
+type GetMemberRequest struct {
+	MemberID             string   `protobuf:"bytes,1,opt,name=memberID,proto3" json:"memberID,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GetMemberRequest) Reset()         { *m = GetMemberRequest{} }
+func (m *GetMemberRequest) String() string { return proto.CompactTextString(m) }
+func (*GetMemberRequest) ProtoMessage()    {}
+func (*GetMemberRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8d0e7a977b094cf8, []int{4}
+}
+
+func (m *GetMemberRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GetMemberRequest.Unmarshal(m, b)
+}
+func (m *GetMemberRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GetMemberRequest.Marshal(b, m, deterministic)
+}
+func (m *GetMemberRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetMemberRequest.Merge(m, src)
+}
+func (m *GetMemberRequest) XXX_Size() int {
+	return xxx_messageInfo_GetMemberRequest.Size(m)
+}
+func (m *GetMemberRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_GetMemberRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GetMemberRequest proto.InternalMessageInfo
+
+func (m *GetMemberRequest) GetMemberID() string {
+	if m != nil {
+		return m.MemberID
+	}
+	return ""
+}
+
+type GetMemberResponse struct {
+	Member               *Member  `protobuf:"bytes,1,opt,name=member,proto3" json:"member,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GetMemberResponse) Reset()         { *m = GetMemberResponse{} }
+func (m *GetMemberResponse) String() string { return proto.CompactTextString(m) }
+func (*GetMemberResponse) ProtoMessage()    {}
+func (*GetMemberResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8d0e7a977b094cf8, []int{5}
+}
+
+func (m *GetMemberResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GetMemberResponse.Unmarshal(m, b)
+}
+func (m *GetMemberResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GetMemberResponse.Marshal(b, m, deterministic)
+}
+func (m *GetMemberResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetMemberResponse.Merge(m, src)
+}
+func (m *GetMemberResponse) XXX_Size() int {
+	return xxx_messageInfo_GetMemberResponse.Size(m)
+}
+func (m *GetMemberResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_GetMemberResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GetMemberResponse proto.InternalMessageInfo
+
+func (m *GetMemberResponse) GetMember() *Member {
+	if m != nil {
+		return m.Member
+	}
+	return nil
+}
+
 type CheckAccessTokenRequest struct {
 	AccessToken          string   `protobuf:"bytes,1,opt,name=accessToken,proto3" json:"accessToken,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -242,7 +320,7 @@ func (m *CheckAccessTokenRequest) Reset()         { *m = CheckAccessTokenRequest
 func (m *CheckAccessTokenRequest) String() string { return proto.CompactTextString(m) }
 func (*CheckAccessTokenRequest) ProtoMessage()    {}
 func (*CheckAccessTokenRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_8d0e7a977b094cf8, []int{4}
+	return fileDescriptor_8d0e7a977b094cf8, []int{6}
 }
 
 func (m *CheckAccessTokenRequest) XXX_Unmarshal(b []byte) error {
@@ -283,7 +361,7 @@ func (m *CheckAccessTokenResponse) Reset()         { *m = CheckAccessTokenRespon
 func (m *CheckAccessTokenResponse) String() string { return proto.CompactTextString(m) }
 func (*CheckAccessTokenResponse) ProtoMessage()    {}
 func (*CheckAccessTokenResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_8d0e7a977b094cf8, []int{5}
+	return fileDescriptor_8d0e7a977b094cf8, []int{7}
 }
 
 func (m *CheckAccessTokenResponse) XXX_Unmarshal(b []byte) error {
@@ -340,7 +418,7 @@ func (m *Cookie) Reset()         { *m = Cookie{} }
 func (m *Cookie) String() string { return proto.CompactTextString(m) }
 func (*Cookie) ProtoMessage()    {}
 func (*Cookie) Descriptor() ([]byte, []int) {
-	return fileDescriptor_8d0e7a977b094cf8, []int{6}
+	return fileDescriptor_8d0e7a977b094cf8, []int{8}
 }
 
 func (m *Cookie) XXX_Unmarshal(b []byte) error {
@@ -375,42 +453,113 @@ func (m *Cookie) GetExpiration() int64 {
 	return 0
 }
 
+type Member struct {
+	ID                   string   `protobuf:"bytes,1,opt,name=ID,proto3" json:"ID,omitempty"`
+	Email                string   `protobuf:"bytes,2,opt,name=email,proto3" json:"email,omitempty"`
+	PublicKey            string   `protobuf:"bytes,3,opt,name=publicKey,proto3" json:"publicKey,omitempty"`
+	PrivateKey           string   `protobuf:"bytes,4,opt,name=privateKey,proto3" json:"privateKey,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Member) Reset()         { *m = Member{} }
+func (m *Member) String() string { return proto.CompactTextString(m) }
+func (*Member) ProtoMessage()    {}
+func (*Member) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8d0e7a977b094cf8, []int{9}
+}
+
+func (m *Member) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Member.Unmarshal(m, b)
+}
+func (m *Member) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Member.Marshal(b, m, deterministic)
+}
+func (m *Member) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Member.Merge(m, src)
+}
+func (m *Member) XXX_Size() int {
+	return xxx_messageInfo_Member.Size(m)
+}
+func (m *Member) XXX_DiscardUnknown() {
+	xxx_messageInfo_Member.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Member proto.InternalMessageInfo
+
+func (m *Member) GetID() string {
+	if m != nil {
+		return m.ID
+	}
+	return ""
+}
+
+func (m *Member) GetEmail() string {
+	if m != nil {
+		return m.Email
+	}
+	return ""
+}
+
+func (m *Member) GetPublicKey() string {
+	if m != nil {
+		return m.PublicKey
+	}
+	return ""
+}
+
+func (m *Member) GetPrivateKey() string {
+	if m != nil {
+		return m.PrivateKey
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*CreateMemberRequest)(nil), "CreateMemberRequest")
 	proto.RegisterType((*CreateMemberResponse)(nil), "CreateMemberResponse")
 	proto.RegisterType((*LoginMemberRequest)(nil), "LoginMemberRequest")
 	proto.RegisterType((*LoginMemberResponse)(nil), "LoginMemberResponse")
+	proto.RegisterType((*GetMemberRequest)(nil), "GetMemberRequest")
+	proto.RegisterType((*GetMemberResponse)(nil), "GetMemberResponse")
 	proto.RegisterType((*CheckAccessTokenRequest)(nil), "CheckAccessTokenRequest")
 	proto.RegisterType((*CheckAccessTokenResponse)(nil), "CheckAccessTokenResponse")
 	proto.RegisterType((*Cookie)(nil), "Cookie")
+	proto.RegisterType((*Member)(nil), "Member")
 }
 
 func init() { proto.RegisterFile("Members.proto", fileDescriptor_8d0e7a977b094cf8) }
 
 var fileDescriptor_8d0e7a977b094cf8 = []byte{
-	// 338 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xbc, 0x53, 0x4d, 0x4f, 0xc2, 0x40,
-	0x14, 0xe4, 0x23, 0x02, 0x3e, 0xd4, 0x98, 0x47, 0x8d, 0x6b, 0x0f, 0x86, 0xec, 0x49, 0x2f, 0x7b,
-	0xc0, 0x9b, 0x46, 0x13, 0x83, 0x51, 0x89, 0x7a, 0xa9, 0xfe, 0x81, 0xa5, 0xbe, 0xc8, 0x86, 0x8f,
-	0xad, 0xbb, 0x05, 0x34, 0xfe, 0x4f, 0x7f, 0x8f, 0x61, 0x4b, 0xb1, 0x15, 0xf0, 0x62, 0xe2, 0x71,
-	0x66, 0xfb, 0xa6, 0x3b, 0xf3, 0x66, 0x61, 0xfb, 0x81, 0x86, 0x5d, 0x32, 0x56, 0x44, 0x46, 0xc7,
-	0x9a, 0xdf, 0x40, 0xa3, 0x6d, 0x48, 0xc6, 0x94, 0xd0, 0x01, 0xbd, 0x8e, 0xc9, 0xc6, 0xe8, 0xc1,
-	0x06, 0x0d, 0xa5, 0x1a, 0xb0, 0x62, 0xb3, 0x78, 0xb4, 0x19, 0x24, 0x00, 0x7d, 0xa8, 0x45, 0xd2,
-	0xda, 0xa9, 0x36, 0xcf, 0xac, 0xe4, 0x0e, 0x16, 0x98, 0x4f, 0xc1, 0xcb, 0x0b, 0xd9, 0x48, 0x8f,
-	0x2c, 0xcd, 0x66, 0x86, 0x8e, 0xe9, 0x5c, 0xcd, 0xc5, 0x16, 0x18, 0x8f, 0xa1, 0x2e, 0xc3, 0x90,
-	0xac, 0x7d, 0xd2, 0x7d, 0x1a, 0x39, 0xc9, 0x7a, 0xab, 0x2a, 0xda, 0x5a, 0xf7, 0x15, 0x05, 0xd9,
-	0x33, 0x64, 0x50, 0xbd, 0x95, 0xb6, 0x77, 0x47, 0xef, 0xac, 0xec, 0x54, 0x52, 0xc8, 0xaf, 0x01,
-	0xef, 0xf5, 0x8b, 0x1a, 0xfd, 0xd5, 0xc0, 0x04, 0x1a, 0x39, 0x9d, 0xff, 0xba, 0xff, 0x19, 0xec,
-	0xb7, 0x7b, 0x14, 0xf6, 0x2f, 0xbf, 0xbf, 0x4e, 0x4d, 0x34, 0xf3, 0xfa, 0xc9, 0xef, 0xb3, 0x14,
-	0xff, 0x00, 0xb6, 0x3c, 0x3c, 0xbf, 0x39, 0x83, 0xaa, 0x1d, 0x3b, 0xde, 0x4d, 0xd6, 0x82, 0x14,
-	0xe6, 0x3c, 0x95, 0x7e, 0xf7, 0x54, 0x5e, 0xef, 0x89, 0x5f, 0x40, 0x25, 0xa1, 0x67, 0x69, 0x4f,
-	0xe4, 0x60, 0x4c, 0x69, 0xda, 0x0e, 0xe0, 0x21, 0x00, 0xbd, 0x45, 0xca, 0xc8, 0x58, 0xe9, 0x24,
-	0x9d, 0x72, 0x90, 0x61, 0x5a, 0x9f, 0x45, 0xd8, 0x99, 0xb7, 0xf1, 0x91, 0xcc, 0x44, 0x85, 0x84,
-	0xe7, 0xb0, 0x95, 0x6d, 0x11, 0x7a, 0x62, 0x45, 0x3b, 0xfd, 0x3d, 0xb1, 0xaa, 0x6a, 0xbc, 0x80,
-	0xa7, 0x50, 0xcf, 0xec, 0x10, 0x1b, 0x62, 0xb9, 0x19, 0xbe, 0x27, 0x56, 0xac, 0x99, 0x17, 0xb0,
-	0x03, 0xbb, 0x3f, 0xa3, 0x44, 0x26, 0xd6, 0xac, 0xc6, 0x3f, 0x10, 0xeb, 0x72, 0xe7, 0x85, 0x6e,
-	0xc5, 0xbd, 0xad, 0x93, 0xaf, 0x00, 0x00, 0x00, 0xff, 0xff, 0x91, 0x1e, 0x2c, 0x49, 0x6c, 0x03,
-	0x00, 0x00,
+	// 429 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xbc, 0x54, 0xcd, 0x6e, 0xd3, 0x40,
+	0x10, 0x76, 0x1d, 0x70, 0x9a, 0x09, 0x54, 0xed, 0xc4, 0x08, 0x63, 0x21, 0xa8, 0xf6, 0x04, 0x97,
+	0x3d, 0x94, 0x9e, 0x40, 0x20, 0xa1, 0x54, 0x94, 0x08, 0xb8, 0x18, 0x5e, 0x60, 0x63, 0x46, 0x74,
+	0x15, 0x27, 0x6b, 0xbc, 0xb6, 0x0b, 0xe2, 0x15, 0x78, 0x68, 0xe4, 0xf5, 0xdf, 0xba, 0x49, 0x7a,
+	0x41, 0xea, 0xf1, 0xfb, 0x66, 0x76, 0x7e, 0xbf, 0x59, 0x78, 0xf8, 0x85, 0xd6, 0x4b, 0xca, 0x34,
+	0x4f, 0x33, 0x95, 0x2b, 0x76, 0x09, 0xb3, 0x79, 0x46, 0x22, 0xa7, 0x9a, 0x8e, 0xe8, 0x67, 0x41,
+	0x3a, 0x47, 0x1f, 0xee, 0xd3, 0x5a, 0xc8, 0x24, 0x38, 0x38, 0x3d, 0x78, 0x31, 0x89, 0x6a, 0x80,
+	0x21, 0x1c, 0xa6, 0x42, 0xeb, 0x6b, 0x95, 0x7d, 0x0f, 0x5c, 0x63, 0xe8, 0x30, 0xbb, 0x06, 0x7f,
+	0x18, 0x48, 0xa7, 0x6a, 0xa3, 0xa9, 0x7a, 0xb3, 0x36, 0xcc, 0xe2, 0xa2, 0x09, 0xd6, 0x61, 0x7c,
+	0x09, 0x53, 0x11, 0xc7, 0xa4, 0xf5, 0x37, 0xb5, 0xa2, 0x8d, 0x09, 0x39, 0x3d, 0x1b, 0xf3, 0xb9,
+	0x52, 0x2b, 0x49, 0x91, 0x6d, 0xc3, 0x00, 0xc6, 0x1f, 0x85, 0xbe, 0xfa, 0x44, 0xbf, 0x83, 0x91,
+	0x89, 0xd2, 0x42, 0xf6, 0x01, 0xf0, 0xb3, 0xfa, 0x21, 0x37, 0xff, 0xdb, 0x40, 0x09, 0xb3, 0x41,
+	0x9c, 0xbb, 0xaa, 0x9f, 0xc3, 0xf1, 0x25, 0xe5, 0xc3, 0xea, 0x6f, 0x49, 0xca, 0xce, 0xe1, 0xc4,
+	0xf2, 0x6f, 0xaa, 0x7c, 0x0e, 0x5e, 0xed, 0x60, 0xdc, 0xab, 0x22, 0x1a, 0x87, 0x86, 0x66, 0x6f,
+	0xe0, 0xf1, 0xfc, 0x8a, 0xe2, 0xd5, 0xfb, 0xbe, 0xa6, 0x36, 0xd9, 0xe9, 0xb0, 0x8b, 0x3a, 0x9f,
+	0x4d, 0xb1, 0x3f, 0x10, 0x6c, 0x3f, 0x6e, 0x32, 0x07, 0x30, 0xd6, 0x85, 0xe1, 0xcd, 0xcb, 0xc3,
+	0xa8, 0x85, 0x83, 0x26, 0xdc, 0xdb, 0x27, 0x37, 0xda, 0x3f, 0x39, 0xf6, 0x0e, 0xbc, 0x9a, 0xae,
+	0x76, 0x5a, 0x8a, 0xa4, 0xa0, 0x76, 0xa7, 0x06, 0xe0, 0x33, 0x00, 0xfa, 0x95, 0xca, 0x4c, 0xe4,
+	0x52, 0xd5, 0x3b, 0x18, 0x45, 0x16, 0xc3, 0x12, 0xf0, 0xea, 0x59, 0xe0, 0x11, 0xb8, 0xdd, 0x3c,
+	0xdd, 0xc5, 0x45, 0xaf, 0x11, 0xd7, 0xd6, 0xc8, 0x53, 0x98, 0xa4, 0xc5, 0x32, 0x91, 0x71, 0xbf,
+	0xab, 0x9e, 0xa8, 0xb2, 0xa5, 0x99, 0x2c, 0x45, 0x4e, 0x95, 0xf9, 0x9e, 0x31, 0x5b, 0xcc, 0xd9,
+	0x5f, 0x17, 0x8e, 0x9a, 0x0b, 0xfb, 0x4a, 0x59, 0x29, 0x63, 0xc2, 0xb7, 0xf0, 0xc0, 0xbe, 0x0c,
+	0xf4, 0xf9, 0x8e, 0x8b, 0x0b, 0x1f, 0xf1, 0x5d, 0xe7, 0xc3, 0x1c, 0x7c, 0x0d, 0x53, 0x4b, 0x97,
+	0x38, 0xe3, 0xdb, 0x6a, 0x0f, 0x7d, 0xbe, 0x43, 0xba, 0xcc, 0xc1, 0x73, 0x98, 0x74, 0x5a, 0xc1,
+	0x13, 0x7e, 0x53, 0x67, 0x21, 0xf2, 0x2d, 0x29, 0x31, 0x07, 0x17, 0x70, 0x7c, 0x73, 0xdd, 0x18,
+	0xf0, 0x3d, 0xf2, 0x09, 0x9f, 0xf0, 0x7d, 0xda, 0x60, 0xce, 0xd2, 0x33, 0xbf, 0xcc, 0xab, 0x7f,
+	0x01, 0x00, 0x00, 0xff, 0xff, 0x38, 0x98, 0x0a, 0x6c, 0x76, 0x04, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -427,6 +576,7 @@ const _ = grpc.SupportPackageIsVersion4
 type MembersServiceClient interface {
 	CreateMember(ctx context.Context, in *CreateMemberRequest, opts ...grpc.CallOption) (*CreateMemberResponse, error)
 	LoginMember(ctx context.Context, in *LoginMemberRequest, opts ...grpc.CallOption) (*LoginMemberResponse, error)
+	GetMember(ctx context.Context, in *GetMemberRequest, opts ...grpc.CallOption) (*GetMemberResponse, error)
 	CheckAccessToken(ctx context.Context, in *CheckAccessTokenRequest, opts ...grpc.CallOption) (*CheckAccessTokenResponse, error)
 }
 
@@ -456,6 +606,15 @@ func (c *membersServiceClient) LoginMember(ctx context.Context, in *LoginMemberR
 	return out, nil
 }
 
+func (c *membersServiceClient) GetMember(ctx context.Context, in *GetMemberRequest, opts ...grpc.CallOption) (*GetMemberResponse, error) {
+	out := new(GetMemberResponse)
+	err := c.cc.Invoke(ctx, "/MembersService/GetMember", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *membersServiceClient) CheckAccessToken(ctx context.Context, in *CheckAccessTokenRequest, opts ...grpc.CallOption) (*CheckAccessTokenResponse, error) {
 	out := new(CheckAccessTokenResponse)
 	err := c.cc.Invoke(ctx, "/MembersService/CheckAccessToken", in, out, opts...)
@@ -469,6 +628,7 @@ func (c *membersServiceClient) CheckAccessToken(ctx context.Context, in *CheckAc
 type MembersServiceServer interface {
 	CreateMember(context.Context, *CreateMemberRequest) (*CreateMemberResponse, error)
 	LoginMember(context.Context, *LoginMemberRequest) (*LoginMemberResponse, error)
+	GetMember(context.Context, *GetMemberRequest) (*GetMemberResponse, error)
 	CheckAccessToken(context.Context, *CheckAccessTokenRequest) (*CheckAccessTokenResponse, error)
 }
 
@@ -481,6 +641,9 @@ func (*UnimplementedMembersServiceServer) CreateMember(ctx context.Context, req 
 }
 func (*UnimplementedMembersServiceServer) LoginMember(ctx context.Context, req *LoginMemberRequest) (*LoginMemberResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method LoginMember not implemented")
+}
+func (*UnimplementedMembersServiceServer) GetMember(ctx context.Context, req *GetMemberRequest) (*GetMemberResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetMember not implemented")
 }
 func (*UnimplementedMembersServiceServer) CheckAccessToken(ctx context.Context, req *CheckAccessTokenRequest) (*CheckAccessTokenResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CheckAccessToken not implemented")
@@ -526,6 +689,24 @@ func _MembersService_LoginMember_Handler(srv interface{}, ctx context.Context, d
 	return interceptor(ctx, in, info, handler)
 }
 
+func _MembersService_GetMember_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetMemberRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MembersServiceServer).GetMember(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/MembersService/GetMember",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MembersServiceServer).GetMember(ctx, req.(*GetMemberRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 func _MembersService_CheckAccessToken_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CheckAccessTokenRequest)
 	if err := dec(in); err != nil {
@@ -555,6 +736,10 @@ var _MembersService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "LoginMember",
 			Handler:    _MembersService_LoginMember_Handler,
+		},
+		{
+			MethodName: "GetMember",
+			Handler:    _MembersService_GetMember_Handler,
 		},
 		{
 			MethodName: "CheckAccessToken",
